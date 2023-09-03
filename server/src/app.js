40abaf68ef160b00cd8e6c76805eefdcde3989ba
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-09-03 20:33:34 +0300

    "auth checked"

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const passport = require('passport'); // PassportJS middleware
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // PassportJS Google OAuth 2.0 strategy
+const cookieParser = require('cookie-parser'); // Express middleware for parsing cookie header and signing cookies. The parsed cookie is available as req.cookies.
+const { encryptCookieNodeMiddleware } = require('encrypt-cookie'); // Express middleware for encrypting and decrypting cookies.
+const jwt = require('express-jwt'); // Express middleware for validating JWTs through the jsonwebtoken module. The decoded JWT payload is available as req.user or req.auth.
+
 const apiRoutes = require('./routes');
 const db = require('./db');
-const passport = require('passport');
+const User = require('./models/User');
 
 const port = process.env.PORT;
 
@@ -24,8 +30,84 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api', apiRoutes);
+// Let Express app use cookie middlewares with secret key for handling encryption of cookies
+app.use(cookieParser(process.env.SECRET_KEY));
+app.use(encryptCookieNodeMiddleware(process.env.SECRET_KEY));
+
+// Let Express app use JWT middleware to be used on all routes starting with /api
+app.use(
+  '/api',
+  jwt({
+    secret: process.env.SECRET_KEY, // secret key is always required
+    algorithms: ['HS256'], // encryption algorithm is always required
+    requestProperty: 'auth', // This ensures that decoded token details will be available on req.auth else req.user is the default.
+    getToken: (req) => req.signedCookies['_t'] ?? req.cookies['_t'],
+  }).unless({ path: ['/api/auth/google', '/api/auth/google/callback'] })
+);
+
+// Let Express app use a middleware function that sends 401 error response code for auth errors and 500 for others
+app.use(function (err, req, res, _next) {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ error: true, message: `Invalid Token: ${err.message}` });
+  } else {
+    res
+      .status(500)
+      .json({ error: true, message: 'Internal server error occured' });
+  }
+});
+
+// Prepare Google Auth handling configuration
+const googleConfigs = {
+  clientID: process.env.GAPP_CLIENT_ID,
+  clientSecret: process.env.GAPP_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/api/auth/google/callback',
+};
+const afterGoogleLogin = async function (
+  accessToken,
+  refreshToken,
+  profile,
+  cb
+) {
+  try {
+    // Search if logged in user exists in the database
+    let user = await User.findOne({ providerId: `google-${profile.id}` });
+    if (!user) {
+      // If user doesn't exist, create the user record on the database
+      // In creating user record we use values shared by Google with us such as email, name, picture, etc.
+      user = await User.create({
+        email: profile.emails?.shift().value,
+        name: profile.displayName,
+        firstname: profile.name?.givenName,
+        lastname: profile.name?.familyName,
+        profilePicture: profile.photos?.shift().value,
+        provider: 'google',
+        providerId: `google-${profile.id}`,
+      });
+    }
+    // Finally return user details
+    cb(null, user.toJSON());
+  } catch (err) {
+    cb(err, null);
+  }
+};
+
+// Configuring PassportJS Google OAuth 2.0 strategy on PassportJS instance
+passport.use(
+  // GoogleStrategy takes 2 arguments.
+  new GoogleStrategy(
+    // First argument is an object with client ID, client secret and callback URL.
+    googleConfigs,
+    // Second argument is a callback function to process authentication completion.
+    afterGoogleLogin
+  )
+);
+
+// Let Express app use configured and initialized PassportJS middleware
 app.use(passport.initialize());
+
+app.use('/api', apiRoutes);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,76 +1,59 @@
 const express = require('express');
 const routes = express.Router();
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); // A NodeJS module to implement JWTs
 
-const User = require('../models/User');
-const { authenticate } = require('../middleware/checkAuth');
-
-// google strategy config
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GAPP_CLIENT_ID,
-      clientSecret: process.env.GAPP_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/api/auth/google/callback',
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      // Find or create user
-      let user = await User.findOne({ googleId: profile.id });
-
-      if (!user) {
-        user = await new User({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          googleId: profile.id,
-        }).save();
-      }
-
-      done(null, user);
-    }
-  )
-);
-
-routes.get(
-  '/api/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email', 'openid'] }),
-  (req, res) => {
-    res.end();
-  }
-);
-
-routes.get('/api/auth/me', authenticate, (req, res) => {
+// GET /api/auth/me : Returns authentication info or user profile info
+routes.get('/me', (req, res) => {
   res.json(req.auth);
 });
 
+// GET /api/auth/google : Executes PassportJS authentication
 routes.get(
-  '/api/auth/google/callback',
-  passport.authenticate('google', { session: false }),
-  (req, res) => {
-    const user = req.user;
-    const token = jwt.sign(
-      {
-        userName: user.userName,
-        email: user.email,
-        providerId: `google-${user.providerId}`,
-      },
-      process.env.SECRET_KEY,
-      { expiresIn: '14d' }
-    );
-    console.log(token, 'tokenn');
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 14 * 24 * 60 * 60 * 1000,
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email', 'openid'] })
+);
+
+// GET /api/auth/google/callback : Callback or Redirect URL after Google authentication completed
+// This is how Google returns control to our application after Google login page
+routes.get(
+  '/google/callback',
+  // In case of failed authentication, redirect to home page. This can also be an error page in some cases.
+  // Also, disable PassportJS sessions since we want to use JWTs.
+  passport.authenticate('google', { failureRedirect: '/', session: false }),
+  function (req, res) {
+    // Fetch and configure user details required to create JWT
+    const cookieAge = 14 * 24 * 3600; // 14 days converted to seconds
+    const { _id, name, email, providerId, profilePicture } = req.user;
+    const payload = {
+      name,
+      email,
+      providerId,
+      avatar: profilePicture,
+    };
+
+    // Create signed JWT using jsonwebtoken package
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: cookieAge,
+      subject: _id.toString(),
     });
 
+    // Set cookie with name '_t' and above signed JWT
+    // Set HTTP only to ensure that cookie is accessible only by the server, this creates additional layer of security on the client end
+    res.cookie('_t', token, {
+      maxAge: cookieAge * 1000,
+      httpOnly: true,
+      signed: true,
+    });
+
+    // Redirect to home page after all authentication and subsequent operations completed
     res.redirect('/');
   }
 );
 
-routes.get('/api/auth/logout', authenticate, (req, res) => {
-  res.clearCookie('token');
+// GET /api/auth/logout : Clear JWT stored in cookies to execute logout operation
+routes.get('/logout', (req, res) => {
+  res.clearCookie('_t');
   res.status(200).json({ success: true });
 });
 
